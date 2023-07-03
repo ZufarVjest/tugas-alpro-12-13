1427d8afc80e54cb761e1582abab2b4c588d9760
ZufarVjest <128177699+ZufarVjest@users.noreply.github.com>
2023-07-03 22:36:53 +0700

    Add files via upload

---
+++ Tugas Pertemuan 12&13/Struct Mahasiswa/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string jurusan;
+    int tahunLulus;
+};
+
+int main() {
+    const int jumlahMahasiswa = 4;
+    Mahasiswa mahasiswa[jumlahMahasiswa];
+
+    // Mengisi data mahasiswa
+    mahasiswa[0] = {"A11.2020.01234", "Ando", "Broadcasting", 2023};
+    mahasiswa[1] = {"A11.2010.01234", "Bodi", "Sistem Informasi", 2013};
+    mahasiswa[2] = {"A11.2000.01234", "Alim", "DKV", 2003};
+    mahasiswa[3] = {"A11.1990.01234", "Siti fatonah", "Kesehatan", 1993};
+
+    // Mencetak data mahasiswa
+    for (int i = 0; i < jumlahMahasiswa; i++) {
+        cout << "Data Mahasiswa " << i + 1 << ":" << endl;
+        cout << "NIM: " << mahasiswa[i].nim << endl;
+        cout << "Nama: " << mahasiswa[i].nama << endl;
+        cout << "Jurusan: " << mahasiswa[i].jurusan << endl;
+        cout << "Tahun Lulus: " << mahasiswa[i].tahunLulus << endl;
+        cout << endl;
+    }
+
+    return 0;
+}
